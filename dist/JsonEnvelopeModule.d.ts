85da945a2726884115fff1d93c0ce84d2f1654e8
Rob Anderson <rob.anderson@ncl.ac.uk>
2019-01-16 17:01:56 +0000

    0.2.0

--- dist/JsonEnvelopeModule.d.ts
+++ dist/JsonEnvelopeModule.d.ts
@@ -1,28 +1,24 @@
 import { Module, ChowChow, BaseContext } from '@robb_j/chowchow';
-import { IApiOptions } from 'api-formatter';
-declare type JsonEnvelopeConfig = IApiOptions;
+declare type JsonEnvelopeConfig = {
+    name?: string;
+    version?: string;
+    handleErrors?: boolean;
+};
 export declare type JsonEnvelopeContext = {
     sendData: (data: any) => void;
     sendFail: (messages: string[], status?: number) => void;
 };
-export declare function makeEnvelope(data: any, success: boolean, messages: string[], status: number, name: string, version: string): {
-    meta: {
-        success: boolean;
-        messages: string[];
-        status: number;
-        name: string;
-        version: string;
-    };
-    data: any;
-};
 export declare class JsonEnvelopeModule implements Module {
     config: JsonEnvelopeConfig;
     app: ChowChow;
+    publicName?: string;
+    publicVersion?: string;
     constructor(config?: JsonEnvelopeConfig);
     checkEnvironment(): void;
     setupModule(): void;
     clearModule(): void;
     extendExpress(): void;
-    extendEndpointContext(ctx: BaseContext): JsonEnvelopeContext;
+    extendEndpointContext({ res }: BaseContext): JsonEnvelopeContext;
+    makeEnvelope(data: any, success?: boolean, messages?: string[], status?: number): any;
 }
 export {};
--- dist/JsonEnvelopeModule.js
+++ dist/JsonEnvelopeModule.js
@@ -1,28 +1,53 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const api_formatter_1 = require("api-formatter");
-function makeEnvelope(data, success, messages, status, name, version) {
-    return {
-        meta: { success, messages, status, name, version },
-        data
-    };
-}
-exports.makeEnvelope = makeEnvelope;
 class JsonEnvelopeModule {
     constructor(config = {}) {
         this.config = config;
         this.app = null;
+        this.publicName = config.name || process.env.npm_package_name;
+        this.publicVersion = config.version || process.env.npm_package_version;
     }
     checkEnvironment() { }
-    setupModule() { }
+    setupModule() {
+        const { handleErrors = false } = this.config;
+        if (handleErrors) {
+            this.app.applyErrorHandler((err, ctx) => {
+                let messages;
+                if (err[Symbol.iterator])
+                    messages = Array.from(err);
+                else if (typeof err === 'string')
+                    messages = [err];
+                else if (err instanceof Error)
+                    messages = [err.message];
+                else
+                    messages = ['An unknown error occurred'];
+                ctx.res.send(this.makeEnvelope(null, false, messages, 400));
+            });
+        }
+    }
     clearModule() { }
     extendExpress() { }
-    extendEndpointContext(ctx) {
-        let api = new api_formatter_1.Api(ctx.req, ctx.res, this.config);
+    extendEndpointContext({ res }) {
+        // let api = new Api(ctx.req, ctx.res, this.config)
         return {
-            sendData: (data) => api.sendData(data),
-            sendFail: (msgs, status = 400) => api.sendFail(msgs, status)
+            sendData: (data) => {
+                res.send(this.makeEnvelope(data, true));
+            },
+            sendFail: (messages, status = 400) => {
+                res.send(this.makeEnvelope(null, false, messages, status));
+            }
         };
+    }
+    makeEnvelope(data, success = true, messages = [], status = 200) {
+        let envelope = {
+            meta: { success, messages, status },
+            data
+        };
+        if (this.publicName)
+            envelope.meta.name = this.publicName;
+        if (this.publicVersion)
+            envelope.meta.version = this.publicVersion;
+        return envelope;
     }
 }
 exports.JsonEnvelopeModule = JsonEnvelopeModule;
