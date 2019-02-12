0edfb32e7e984f202d45976139144f7ccd1127f8
Rob Anderson <rob@andrsn.uk>
2019-02-12 22:14:49 +0000

    :white_check_mark: Add tests

--- src/__test__/JsonEnvelopeModule.spec.ts
+++ src/__test__/JsonEnvelopeModule.spec.ts
@@ -1,12 +1,80 @@
-import { JsonEnvelopeModule } from '../JsonEnvelopeModule'
+import { JsonEnvelopeModule, processError } from '../JsonEnvelopeModule'
+import { ChowChow, ChowChowInternals } from '@robb_j/chowchow'
+
+class FakeChow extends ChowChow {
+  async startServer() {}
+  async stopServer() {}
+}
+
+describe('#processError', () => {
+  it('should process iterators', () => {
+    const message = processError(['A', 'B', 'C'])
+    expect(message).toEqual(['A', 'B', 'C'])
+  })
+  it('should process strings', () => {
+    const message = processError('A')
+    expect(message).toEqual(['A'])
+  })
+  it('should process an Error', () => {
+    const message = processError(new Error('A'))
+    expect(message).toEqual(['A'])
+  })
+})
 
 describe('JsonEnvelopeModule', () => {
+  let chow: FakeChow & ChowChowInternals
   let jsonEnvelope: JsonEnvelopeModule
   beforeEach(() => {
-    jsonEnvelope = new JsonEnvelopeModule()
+    jsonEnvelope = new JsonEnvelopeModule({
+      handleErrors: true,
+      name: 'my-fancy-api',
+      version: 'v1'
+    })
+    chow = FakeChow.create().use(jsonEnvelope) as any
+    jsonEnvelope.app = chow
   })
 
-  it('should exist', async () => {
-    expect(jsonEnvelope).toBeDefined()
+  describe('#setupModule', () => {
+    it('should apply the error handler', async () => {
+      await jsonEnvelope.setupModule()
+      expect(chow.errorHandlers).toHaveLength(1)
+    })
+  })
+
+  describe('#extendEndpointContext', () => {
+    const baseCtx = { res: {} } as any
+    it('should add a #sendData method', () => {
+      const ctx = jsonEnvelope.extendEndpointContext(baseCtx)
+      expect(ctx.sendData).toBeInstanceOf(Function)
+    })
+    it('should add a #sendFail method', () => {
+      const ctx = jsonEnvelope.extendEndpointContext(baseCtx)
+      expect(ctx.sendFail).toBeInstanceOf(Function)
+    })
+  })
+
+  describe('#makeEnvelope', () => {
+    let env: any
+    beforeEach(() => {
+      env = jsonEnvelope.makeEnvelope({ name: 'tim' }, true, ['Hey'], 418)
+    })
+
+    it('should have a meta packet', () => {
+      expect(env.meta).toMatchObject({
+        success: true,
+        messages: ['Hey']
+      })
+    })
+    it('should have a data payload', () => {
+      expect(env.data).toMatchObject({
+        name: 'tim'
+      })
+    })
+    it('should have the name and version', () => {
+      expect(env.meta).toMatchObject({
+        name: 'my-fancy-api',
+        version: 'v1'
+      })
+    })
   })
 })
